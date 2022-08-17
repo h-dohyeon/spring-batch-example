32b22c0338103466369356a72ad1bdd2b5f4197e
huray-jieun <jieun.hong@huray.net>
2022-08-17 16:17:19 +0900

    Add Spring Cloud Stream Event Job Lancher

--- src/main/java/net/huray/platform/springbatchexample/SpringBatchExampleApplication.java
+++ src/main/java/net/huray/platform/springbatchexample/SpringBatchExampleApplication.java
@@ -1,13 +1,52 @@
 package net.huray.platform.springbatchexample;
 
+import lombok.extern.slf4j.Slf4j;
+import net.huray.platform.springbatchexample.domain.person.dto.MyMessage;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableAsync;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
+import java.util.function.Function;
+
+@Slf4j
 @SpringBootApplication
+@EnableAsync
 public class SpringBatchExampleApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBatchExampleApplication.class, args);
+    }
+
+
+    @Autowired
+    private JobLauncher jobLauncher;
+
+    @Autowired
+    private Job extractionPersonNameJob;
+
+
+    @Bean
+    public Function<Flux<MyMessage>, Mono<Void>> direct() {
+        return myMessageFlux -> myMessageFlux.doOnNext((myMessage) -> {
+                            log.info("##### message for direct : {}", myMessage.getMessage());
+                            try {
+                                jobLauncher.run(extractionPersonNameJob, new JobParametersBuilder()
+                                        .addString("message", myMessage.getMessage())
+                                        .toJobParameters());
+                            } catch (Exception e) {
+                                log.error(e.getMessage());
+                            }
+                        }
+                )
+                .then();
     }
 
 }
--- src/main/java/net/huray/platform/springbatchexample/domain/person/step/PersonStepConfig.java
+++ src/main/java/net/huray/platform/springbatchexample/domain/person/step/PersonStepConfig.java
@@ -1,18 +1,22 @@
 package net.huray.platform.springbatchexample.domain.person.step;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import net.huray.platform.springbatchexample.domain.person.dto.PersonDto;
 import net.huray.platform.springbatchexample.global.error.GlobalExceptionHandler;
 import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.item.ItemProcessor;
 import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.batch.item.file.FlatFileParseException;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@Slf4j
 @Configuration
 @RequiredArgsConstructor
 public class PersonStepConfig {
@@ -27,7 +31,9 @@ public class PersonStepConfig {
     private final Tasklet closeTxtWriterTasklet;
 
     @Bean
-    public Step fillTxtStep() {
+    @JobScope
+    public Step fillTxtStep(@Value("#{jobParameters['message']}")String message) {
+        log.info("##### JOB message for direct : {}", message);
         return stepBuilderFactory.get("fillTxtStep")
                 .<PersonDto, String> chunk(10)
                 .reader(csvReader)
